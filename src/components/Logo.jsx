5e1a8c6b20c06cf9ce90f7acef9eca884d027c0a
tanzir ibne ali <tanziribneali@gmail.com>
2024-01-27 21:30:49 +0600

    delete unUsed file

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = ({ ...props }) => {
   return (
     <div>
-      <img src="../../public/logo.svg" {...props} width={96} alt="Logo" />
+      <img src="/logo.svg" {...props} width={96} alt="Logo" />
     </div>
   );
 };
